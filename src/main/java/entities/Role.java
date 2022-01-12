7843893648e2b162af7b497f82bed4a8a7c1edd4
Janussr <Janusstivang@gmail.com>
2022-01-12 12:32:39 +0100

    test

--- src/main/java/entities/Role.java
+++ src/main/java/entities/Role.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "roles")
 public class Role implements Serializable {
-
+ 
     private static final long serialVersionUID = 1L;
     @Id
     @Basic(optional = false)
